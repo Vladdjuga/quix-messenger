704918376919ad5ea1da25b36cbd8dfc2b7193fa
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2025-06-21 20:23:04 +0200

    minor changes

--- message-service/Grpc/obj/Debug/net9.0/Grpc.AssemblyInfo.cs
+++ message-service/Grpc/obj/Debug/net9.0/Grpc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991d3163dd60415cbf731463b6a3adcd3e1dc950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c43eb05d85bf3028ce8f6daaf453549ab815e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- message-service/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ message-service/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991d3163dd60415cbf731463b6a3adcd3e1dc950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c43eb05d85bf3028ce8f6daaf453549ab815e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
