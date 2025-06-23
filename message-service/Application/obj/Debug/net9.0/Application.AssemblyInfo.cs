a04072c55d945270f114dfc13156390b44f910c4
Vladdjuga <vladdjuga@gmail.com>
2025-06-23 13:20:50 +0300

    Changed configuration a little bit, for a better work with Docker.

--- message-service/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ message-service/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704918376919ad5ea1da25b36cbd8dfc2b7193fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3107545c146563759644934c931cc0218224af3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- message-service/UI/obj/Debug/net9.0/UI.AssemblyInfo.cs
+++ message-service/UI/obj/Debug/net9.0/UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704918376919ad5ea1da25b36cbd8dfc2b7193fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3107545c146563759644934c931cc0218224af3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
