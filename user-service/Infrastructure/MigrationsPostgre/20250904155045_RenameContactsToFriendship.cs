c7259575162cdf07a5904ebeefb406fbebff2440
Vladdjuga <vladdjuga@gmail.com>
2025-09-04 19:03:38 +0300

    refactor: rename user_contacts to friendships and update related database schema

---
+++ user-service/Infrastructure/MigrationsPostgre/20250904155045_RenameContactsToFriendship.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.MigrationsPostgre
+{
+    /// <inheritdoc />
+    public partial class RenameContactsToFriendship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_contacts");
+
+            migrationBuilder.CreateTable(
+                name: "friendships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FriendId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    PrivateChatId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_friendships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_friendships_chats_PrivateChatId",
+                        column: x => x.PrivateChatId,
+                        principalTable: "chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_friendships_users_FriendId",
+                        column: x => x.FriendId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_friendships_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_friendships_FriendId",
+                table: "friendships",
+                column: "FriendId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_friendships_PrivateChatId",
+                table: "friendships",
+                column: "PrivateChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_friendships_UserId_FriendId",
+                table: "friendships",
+                columns: new[] { "UserId", "FriendId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "friendships");
+
+            migrationBuilder.CreateTable(
+                name: "user_contacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    ContactId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PrivateChatId = table.Column<Guid>(type: "uuid", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContactStatus = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_user_contacts_chats_PrivateChatId",
+                        column: x => x.PrivateChatId,
+                        principalTable: "chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_contacts_users_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_user_contacts_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_contacts_ContactId",
+                table: "user_contacts",
+                column: "ContactId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_contacts_PrivateChatId",
+                table: "user_contacts",
+                column: "PrivateChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_contacts_UserId_ContactId",
+                table: "user_contacts",
+                columns: new[] { "UserId", "ContactId" },
+                unique: true);
+        }
+    }
+}
