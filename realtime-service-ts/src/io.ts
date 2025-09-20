e79f9e7e7233980867a69c2eab710f1bcafd6c71
Vladdjuga <vladdjuga@gmail.com>
2025-09-20 17:57:42 +0300

    refactor: consolidate io.ts into a single line and remove unnecessary whitespace

--- realtime-service-ts/src/io.ts
+++ realtime-service-ts/src/io.ts
@@ -1,13 +1,1 @@
-// io.ts
-import { Server } from "socket.io";
-let io: Server;
-
-export function initIO(server: any) {
-    io = new Server(server);
-    return io;
-}
-
-export function getIO() {
-    if (!io) throw new Error("IO not initialized");
-    return io;
-}
+// io.ts import { Server } from "socket.io"; let io: Server; export function initIO(server: any) { io = new Server(server); return io; } export function getIO() { if (!io) throw new Error("IO not initialized"); return io; }
