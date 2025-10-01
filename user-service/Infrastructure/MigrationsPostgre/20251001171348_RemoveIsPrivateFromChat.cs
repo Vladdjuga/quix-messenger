7c6e69715a209d507592b6f0305c3927cb31e043
Vladdjuga <vladdjuga@gmail.com>
2025-10-01 20:14:28 +0300

    feat: Remove 'IsPrivate' column from chats table and update model snapshot

---
+++ user-service/Infrastructure/MigrationsPostgre/20251001171348_RemoveIsPrivateFromChat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.MigrationsPostgre
+{
+    /// <inheritdoc />
+    public partial class RemoveIsPrivateFromChat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrivate",
+                table: "chats");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrivate",
+                table: "chats",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
